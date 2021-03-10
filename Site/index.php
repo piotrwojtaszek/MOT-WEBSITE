1cbfef50b1f7ac592776324f8b875c932df020e1
Piotr Wojtaszek <48589767+piotrwojtaszek@users.noreply.github.com>
2021-03-10 23:49:51 +0100

    From css

--- Site/index.php
+++ Site/index.php
@@ -9,6 +9,10 @@
     <title>Document</title>
 </head>
 <body>
+<div class="holder">
+    <div class="adreskontaktwy">
+        <p>piowoj@piowoj.ct8.pl</p> 
+    </div>
     <ul class="topnav">
         <li><a href="#kontakt">Kontakt</a></li>
         <li><a href="#zaufali">Zaufali nam</a></li>
@@ -17,21 +21,60 @@
         <li><a href="#instrukcja">Instrukcja gry</a></li>
         <li><a href="#idea">Idea gry</a></li>
     </ul>
+</div>
+
     <div class="wrapper">
-
-        <video id="videoBG" autoplay muted loop>
-            <source src="media/vid/bgloop.mp4" type="video/mp4">
-        </video>
-
+        <div class="vid">
+            <video id="videoBG" autoplay muted loop>
+                    <source src="media/vid/bgloop.mp4" type="video/mp4">
+            </video>
+        </div>
         <section id="idea">
             <h4 class="section-title">IDEA GRY</h4>
-            <div class="div-left">
+            <div>
+                <h4>
+                    BÓL
+                </h4>
                 <p>
-                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis nobis est pariatur expedita repudiandae cumque unde quaerat, consequuntur sapiente. Dolorem veritatis molestiae eaque sed! Temporibus vitae totam eius nemo ratione tempore sunt fugiat autem. Et, ullam sunt cupiditate doloremque provident reprehenderit nam beatae, voluptatum praesentium illum ipsum fugit. Sunt, rerum.
+                Ból jest nieprzyjemnym doznaniem zmysłowym i emocjonalnym, które odczuwają już
+niemowlęta. Doświadczanie bólu jest subiektywne i zindywidualizowane, a tolerancja na ból rozwija
+się od wczesnego dzieciństwa – od pierwszych doznawanych obrażeń ciała 1 . Ból wywołany
+medycznymi procedurami często wiąże się z doświadczaniem silnego stresu, lęku, strachu,
+niepewności, obawy czy bezradności. Nieleczony ból i stres podczas zabiegów może wywoływać
+negatywne nastawienie do badania, wzbudzać lęk, zniechęcać do zabiegów medycznych, a w
+przyszłości - powodować unikanie badań czy leczenia z powodu strachu przed nakłuciem 2 . Poza
+negatywnym wpływem na dziecko – ból i niepokój dziecka sprawiają, że zabiegi z użyciem igły są
+bardzo stresujące dla rodziców dziecka i pracowników służby zdrowia 3 .
                 </p>
             </div>
-            <div class="div-right">
-                <img class="image1"src="media/img/screen1.png" alt="Screen">
+            <div>
+                <h4>
+                ODCZUWANIE BÓLU PRZEZ DZIECI
+                </h4>
+                <p>
+                Dzieci od urodzenia narażone są na ból
+spowodowany nakłuciem igłą. U zdrowych dzieci jest to najczęściej szczepienie, które może być
+wykonywane nawet kilkanaście razy do 18 roku życia. U dzieci wymagających leczenia zdarza się to
+jeszcze częściej w związku z pobieraniem krwi, zastrzykami czy wkłuciami dożylnymi.
+                </p>
+            </div>
+            <div>
+                <h4>
+                BADANIA NAD REDUKCJĄ BÓLU W IPS VR4HEALTH LAB (co już wiemy z
+własnych badań)
+                </h4>
+                <p>
+                Naukowcy z Instytutu Psychologii Uniwersytetu Wrocławskiego od prawie dekady
+prowadzą badania nad odwracaniem uwagi od krótkotrwałego bólu. Wyniki ostatnich badań wykazały,
+że autorska gra wymagająca jednoczesnego śledzenia wielu obiektów (mocno angażująca uwagę
+dziecka) zmniejszyła natężenie bólu o 59% w porównaniu z grupą kontrolną, a poziom stresu o 73% w
+grupie wszystkich 39 dzieci 6 . Wyniki te zostały później powielone z podobnymefektem w kolejnym
+badaniu (w grupie 57 dzieci), w którym dodatkowo porównywano dwa rodzaje interwencji w
+wirtualnej rzeczywistości (VR). W jednej dzieci aktywnie grały w analogiczną grę w VR, a w drugiej -
+oglądały film podobny do prezentowanej gry. Obie grupy VR zgłosiły istotnie mniejsze nasilenie bólu
+i stresu w porównaniu do grupy kontrolnej. Natomiast między grupami VR zauważono tendencję do
+większej redukcji bólu i stresu u osób aktywnie grających podczas zabiegu 7 .
+                </p>
             </div>
         </section>
         <section id="instrukcja">
@@ -85,17 +128,30 @@
         <section id="kontakt">
             <h4 class="section-title">KONTAKT</h4>
             <div class="div-left">
-                <p>
+                <h1>
                             NAPISZ DO NAS
-                </p>
+                </h1>
             </div>
             <div class="div-right">
-                <form class="contact-form "action="contactform.php" method="POST">
-                    <input type="text" name="name" placeholder="Full name">
-                    <input type="text" name="mail" placeholder="Your e-mail">
-                    <textarea name="message" placeholder="Message"></textarea>
-                    <button type="submit" name="submit">SEND MAIL</button>
-                </form>
+                <div class="kontakt-form">
+                    <form class="contact-form "action="contactform.php" method="POST">
+                        <div>
+                            <input class="name" type="text" name="name" placeholder="Full name">
+                        </div>
+                        <div>
+                            <input class="mail" type="text" name="mail" placeholder="Your e-mail">
+                        </div>
+                        <div>
+                            <input class="mail" type="text" name="mail" placeholder="Your e-mail">
+                        </div>
+                        <div>
+                            <textarea class="message" name="message" placeholder="Message"></textarea>
+                        </div>
+                        <div>
+                            <button class="submit-btn" type="submit" name="submit">SEND MAIL</button>
+                        </div>
+                    </form>
+                </div>  
             </div>
         </section> 
 </body>
