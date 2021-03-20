2507ae0464592c408b1cd55386177e4492eae1f7
Piotr Wojtaszek <48589767+piotrwojtaszek@users.noreply.github.com>
2021-03-20 18:11:48 +0100

    Navbar

--- Site/index.php
+++ Site/index.php
@@ -3,220 +3,32 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
     
     <title>Document</title>
 </head>
 <body>
-<div class="holder">
-    <div class="adreskontaktwy">
-        <p>piowoj@piowoj.ct8.pl</p> 
-    </div>
-    <ul class="topnav">
-        <li><a href="#idea">Idea gry</a></li>
-        <li><a href="#instrukcja">Instrukcja gry</a></li>
-        <li><a href="#dlaczego">Dlaczego warto</a></li>
-        <li><a href="#onas">O nas</a></li>
-        <li><a href="#zaufali">Zaufali nam</a></li>
-        <li><a href="#kontakt">Kontakt</a></li>
-        <img src="media/multi/UWRlogo.png" alt="" class="uwrlogo">
-    </ul>
-   
-</div>
-
+    <header>
+        <img class="logo" src="media/multi/logo.png"></img>
+        <nav class="nav_links">
+            <li><a href="#section_1">O grze</a></li>
+            <li><a href="#section_2">Instrukcja gry</a></li>
+            <li><a href="#section_3">Dlaczego warto</a></li>
+            <li><a href="#section_4">O bólu</a></li>
+            <li><a href="#section_5">O nas</a></li>
+            <li><a href="#section_6">Zaufali nam</a></li>
+            <li><a href="#section_7">Kontakt</a></li>
+        </nav>
+    </header>
     <div class="wrapper">
-        <div class="vid">
-            <video id="videoBG" playsinline autoplay muted loop>
-                    <source src="media/multi/bgloop.mp4" type="video/mp4">
-            </video>
-            <div class="centered">
-                <p>Magic Spheres</p>
-                <div></div>
-            </div>
-        </div>
-        <section id="idea">
-            <h4 class="section-title">IDEA GRY</h4>
-            <div>
-                <h4>
-                    BÓL
-                </h4>
-                <p>
-                Ból jest nieprzyjemnym doznaniem zmysłowym i emocjonalnym, które odczuwają już
-niemowlęta. Doświadczanie bólu jest subiektywne i zindywidualizowane, a tolerancja na ból rozwija
-się od wczesnego dzieciństwa – od pierwszych doznawanych obrażeń ciała 1 . Ból wywołany
-medycznymi procedurami często wiąże się z doświadczaniem silnego stresu, lęku, strachu,
-niepewności, obawy czy bezradności. Nieleczony ból i stres podczas zabiegów może wywoływać
-negatywne nastawienie do badania, wzbudzać lęk, zniechęcać do zabiegów medycznych, a w
-przyszłości - powodować unikanie badań czy leczenia z powodu strachu przed nakłuciem 2 . Poza
-negatywnym wpływem na dziecko – ból i niepokój dziecka sprawiają, że zabiegi z użyciem igły są
-bardzo stresujące dla rodziców dziecka i pracowników służby zdrowia 3 .
-                </p>
-            </div>
-            <div>
-                <h4>
-                ODCZUWANIE BÓLU PRZEZ DZIECI
-                </h4>
-                <p>
-                Dzieci od urodzenia narażone są na ból
-spowodowany nakłuciem igłą. U zdrowych dzieci jest to najczęściej szczepienie, które może być
-wykonywane nawet kilkanaście razy do 18 roku życia. U dzieci wymagających leczenia zdarza się to
-jeszcze częściej w związku z pobieraniem krwi, zastrzykami czy wkłuciami dożylnymi.
-                </p>
-            </div>
-            <div>
-                <h4>
-                METODY REDUKCJI BÓLU
-                </h4>
-                <p>
-                Ból i stres związany z zabiegami igłowymi można
-zmniejszać przy pomocy różnych metod. Spośród interwencji psychologicznych potwierdzono
-skuteczność w stosowaniu dystrakcji bólu (odwróceniu uwagi od bólu), hipnozy, połączenia technik
-relaksującego oddechu i innych technik pochodzących z terapii poznawczo-behawioralnej 4 . W
-przypadku planowych zabiegów z igłą zaleca się, aby każdemu dziecku zapewnić jednocześnie kilka
-różnych form obniżania bólu, takich jak: znieczulenie miejscowe, komfortową pozycję, dobrane do
-wieku odwrócenie uwagi oraz w przypadku niemowląt karmienie piersią lub podanie słodkiego płynu
-do picia np. z sacharozą 5 .
-                </p>
-            </div>
-            <div>
-                <h4>
-                BADANIA NAD REDUKCJĄ BÓLU W IPS VR4HEALTH LAB (co już wiemy z
-własnych badań)
-                </h4>
-                <p>
-                Naukowcy z Instytutu Psychologii Uniwersytetu Wrocławskiego od prawie dekady
-prowadzą badania nad odwracaniem uwagi od krótkotrwałego bólu. Wyniki ostatnich badań wykazały,
-że autorska gra wymagająca jednoczesnego śledzenia wielu obiektów (mocno angażująca uwagę
-dziecka) zmniejszyła natężenie bólu o 59% w porównaniu z grupą kontrolną, a poziom stresu o 73% w
-grupie wszystkich 39 dzieci 6 . Wyniki te zostały później powielone z podobnymefektem w kolejnym
-badaniu (w grupie 57 dzieci), w którym dodatkowo porównywano dwa rodzaje interwencji w
-wirtualnej rzeczywistości (VR). W jednej dzieci aktywnie grały w analogiczną grę w VR, a w drugiej -
-oglądały film podobny do prezentowanej gry. Obie grupy VR zgłosiły istotnie mniejsze nasilenie bólu
-i stresu w porównaniu do grupy kontrolnej. Natomiast między grupami VR zauważono tendencję do
-większej redukcji bólu i stresu u osób aktywnie grających podczas zabiegu 7 .
-                </p>
-            </div>
-            <div>
-                <h4>
-                IDEA GRY
-                </h4>
-                <p>
-                Widząc efekty naszych badań i potencjał autorskiej gry w VR opracowaliśmy
-grę Magic Spheres, która stanowi odpowiedź na potrzeby leczenia bólu u dzieci podczas zabiegów
-igłowych. Naukowcy wciąż sprawdzają czy zastosowanie VR jest skuteczniejsze od stosowania
-innych technologii cyfrowych 8, 9 . Jednak przeciwbólową skuteczność - podczas procedur pobierania
-krwi -potwierdzono zarówno dla gier w VR 10, 11 , jak i przy użyciu innych technologii cyfrowych 12, 13 .
-Mając na celu możliwie najprostszą formę korzystania z gry, została ona zaprogramowana specjalnie
-do użytku na smartfonie czy tablecie, tak aby zarówno rodzice, jak i pracownicy służby zdrowia mieli
-do niej nieograniczony i niezależny dostęp.<br><br>
-W Magic Spheres wykorzystano mechanizm przeciwbólowego działania gry, który opiera się
-na odwróceniu uwagi dziecka od bodźców bólowych. Efekt przeciwbólowy jest tym silniejszy, im
-bardziej uwaga dziecka zaangażowana jest w bodźce, które odciągają uwagę od zabiegu. Aby osiągnąć
-ten cel, gra została opracowana w oparciu o wspomnianą już zasadę śledzenia wielu obiektów, która
-bardziej niż inne gry angażuje uwagę dziecka. Ta metoda różni się od większości innych, które zostały
-zastosowane do badania rozpraszania uwagi. Po pierwsze, wymaga ciągłej, nieprzerwanej uwagi, a nie
-krótkotrwałych przesunięć uwagi. Po drugie, wymaga jednoczesnego zwracania uwagi na wiele
-obiektów równocześnie 14 . Metoda śledzenia wielu obiektów została już przetestowana podczas badania
-uwagi w różnych grupach wiekowych, w tym u małych dzieci 15 .<br><br>
-Zaprojektowaliśmy grę w taki sposób, aby umożliwić jej zastosowanie dla dzieci w różnym
-wieku. Magic Spherespolega na zapamiętaniu kilku krótko migających elementów, a następnie
-odnalezieniu ich wśród innych poruszających się obiektów. Gra składa się z kilku scen, a każda scena
-ma inne tło i liczbę obiektów latających, aby z każdym poziomem podtrzymywać zainteresowanie
-dziecka i zwiększać szansę na odwrócenie uwagi od bólu.<br><br>
-Gra opracowana jest specyficznie pod kątem zastosowania jej podczas krótkotrwałego,
-bolesnego zabiegu, dlatego zachęcamy, żeby tylko do tych celów była stosowana: jako kilkuminutowe
-przećwiczenie gry bezpośrednio przed zabiegiem oraz przez kilka minut trwania zabiegu.
-Umożliwi to zachowanie efektu nowości i maksymalizację skupienia uwagi dziecka na
-wykonywanym w grze zadaniu – im lepiej dziecko opanuje grę, tym słabszy może być efekt jej
-działania w odwracaniu uwagi od bólu.<br><br>
-Przyświeca nam idea rozpowszechniania wyników badań na rzecz poprawy dobrostanu
-psychicznego dzieci, rodziców i pracowników służby zdrowia. Proponowana przez nas gra jest
-darmowa,dostępna do pobrania w sklepie Google Play (do zainstalowania w systemie Android).
-Korzystanie z gry nie wiąże się z udziałem w badaniu naukowym. Dla nas jako badaczy
-satysfakcjonująca jest świadomość, że gra może pomóc dzieciom przetrwać bolesne zabiegi medyczne
-i korzystnie wpłynąć na ich przyszłe nastawienie do zabiegów medycznych.
-                </p>
-            </div>
-        </section>
-        <section id="testuj">
-            <h4 class="section-title">Przetestuj/sprawdź grę/Zagraj</h4>
-            <div class="store-icon">
-                <img src="media/multi/Store Icon.png" alt="Play Store Icon">
-            </div>
-            <div class="store-button">
-                <a href='https://play.google.com/store/apps/details?id=com.UWr.MagicSpheres&hl=pl&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'><img alt='pobierz z Google Play' src='https://play.google.com/intl/pl/badges/static/images/badges/pl_badge_web_generic.png'/></a>
-            </div>
-        </section>
-
-        <section id="instrukcja">
-            <h4 class="section-title">INSTRUKCJA GRY</h4>
-                <video class="vid-tutorial" controls playsinline autoplay muted loop>
-                        <source src="media/pl/Tutorial.mp4" type="video/mp4">
-                </video>
-        </section>
-        <section id="dlaczego">
-            <h4 class="section-title">DLACZEGO WARTO</h4>
-            <div class="div-left">
-                <p>
-                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis nobis est pariatur expedita repudiandae cumque unde quaerat, consequuntur sapiente. Dolorem veritatis molestiae eaque sed! Temporibus vitae totam eius nemo ratione tempore sunt fugiat autem. Et, ullam sunt cupiditate doloremque provident reprehenderit nam beatae, voluptatum praesentium illum ipsum fugit. Sunt, rerum.
-                </p>
-            </div>
-            <div class="div-right">
-                <img class="image1"src="media/multi/screen1.png" alt="Screen">
-            </div>
-        </section>
-        <section id="onas">
-            <h4 class="section-title">O NAS</h4>
-            <div class="div-left">
-                <p>
-                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis nobis est pariatur expedita repudiandae cumque unde quaerat, consequuntur sapiente. Dolorem veritatis molestiae eaque sed! Temporibus vitae totam eius nemo ratione tempore sunt fugiat autem. Et, ullam sunt cupiditate doloremque provident reprehenderit nam beatae, voluptatum praesentium illum ipsum fugit. Sunt, rerum.
-                </p>
-            </div>
-            <div class="div-right">
-                <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto perferendis veniam delectus sint qui! Blanditiis, ea temporibus, minima cum magni dolor inventore, explicabo doloribus soluta veniam officiis et reiciendis suscipit.
-                </p> 
-            </div>
-        </section>
-        <section id="zaufali">
-            <h4 class="section-title">ZAUFALI NAM</h4>
-            <div class="div-left">
-                <p>
-                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis nobis est pariatur expedita repudiandae cumque unde quaerat, consequuntur sapiente. Dolorem veritatis molestiae eaque sed! Temporibus vitae totam eius nemo ratione tempore sunt fugiat autem. Et, ullam sunt cupiditate doloremque provident reprehenderit nam beatae, voluptatum praesentium illum ipsum fugit. Sunt, rerum.
-                </p>
-            </div>
-            <div class="div-right">
-                <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto perferendis veniam delectus sint qui! Blanditiis, ea temporibus, minima cum magni dolor inventore, explicabo doloribus soluta veniam officiis et reiciendis suscipit.
-                </p> 
-            </div>
-        </section> 
-        <section id="kontakt">
-            <h4 class="section-title">KONTAKT</h4>
-            <div class="div-left">
-                <h1 class="napiszdonas">
-                            NAPISZ DO NAS
-                </h1>
-            </div>
-            <div class="div-right">
-                <div class="kontakt-form">
-                    <form class="contact-form "action="contactform.php" method="POST">
-                        <div>
-                            <input class="name" type="text" name="name" placeholder="Full name">
-                        </div>
-                        <div>
-                            <input class="mail" type="text" name="mail" placeholder="Your e-mail">
-                        </div>
-                        <div>
-                            <textarea class="message" name="message" placeholder="Message"></textarea>
-                        </div>
-                        <div>
-                            <button class="submit-btn" type="submit" name="submit">SEND MAIL</button>
-                        </div>
-                    </form>
-                </div>  
-            </div>
-        </section> 
+        <section id="section_1"></section>
+        <section id="section_2"></section>
+        <section id="section_3"></section>
+        <section id="section_4"></section>
+        <section id="section_5"></section>
+        <section id="section_6"></section>
+        <section id="section_7"></section>
+    </div>
 </body>
 </html>
