dfc2e981b4f9f473ffe95551596d9015edf2e778
Piotr Wojtaszek <48589767+piotrwojtaszek@users.noreply.github.com>
2021-03-20 19:07:47 +0100

    Responsive and sections

--- Site/index.php
+++ Site/index.php
@@ -21,8 +21,20 @@
             <li><a href="#section_7">Kontakt</a></li>
         </nav>
     </header>
+    <section id="section_0">
+            <video class="bg_loop" playsinline autoplay muted loop>
+                        <source src="media/multi/gameplay_loop.mp4" type="video/mp4">
+                </video>
+        </section>  
     <div class="wrapper">
-        <section id="section_1"></section>
+        <section id="section_1">
+            <h1>MAGIC SPHERES</h1>
+            <h4>gra rozpraszająca uwagę dziecka</h4>
+            <p>
+            Pomóż dziecku podczas zabiegu z nakłuciem igłą! Zmień nastawienie dziecka do zabiegów medycznych! Możesz użyć do tego swojego smartfona już kilka minut przed zabiegiem i w trakcie zabiegu nakłucia igłą. Aplikacja <b>Magic Spheres</b> ma pięć scenerii i kilka poziomów trudności, które<b> zaprojektowano, aby odwrócić uwagę dziecka od bólu. </b>
+Pobierz aplikację Magic Spheres i <b>sprawdź, jak rozproszenie uwagi dziecka od ukłucia igłą, zmniejsza jego ból i stres. </b>
+            </p>
+        </section>
         <section id="section_2"></section>
         <section id="section_3"></section>
         <section id="section_4"></section>
