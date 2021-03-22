406d7d904a430783dbb4804561bcf8f42812cc23
Piotr Wojtaszek <48589767+piotrwojtaszek@users.noreply.github.com>
2021-03-22 17:31:50 +0100

    Better now
    Zmiana podejscia do filmu w tle

--- Site/index.php
+++ Site/index.php
@@ -3,61 +3,58 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
-    
-    <title>Document</title>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;500;700;900&family=Roboto:wght@100;300;400;500;700&display=swap" rel="stylesheet">
+    <title>Magic Spheres</title>
 </head>
 <body>
-    <header>
-        <img class="logo" src="media/multi/logo.png"></img>
-        <nav class="nav_links">
-            <li><a href="#section_1">O grze</a></li>
-            <li><a href="#section_2">Instrukcja gry</a></li>
-            <li><a href="#section_3">Dlaczego warto</a></li>
-            <li><a href="#section_4">O bólu</a></li>
-            <li><a href="#section_5">O nas</a></li>
-            <li><a href="#section_6">Zaufali nam</a></li>
-            <li><a href="#section_7">Kontakt</a></li>
-        </nav>
-    </header>
-    <section id="section_1">
-        <div id="bg_loop">
-            <video playsinline autoplay muted loop>
-                <source src="media/multi/gameplay_loop.mp4" type="video/mp4">
-            </video>
-            <div class="video_text">
-                <h1>MAGIC SPHERES</h1>
-                <h2>gra rozpraszająca uwagę dziecka</h2>
-                <p>Pomóż dziecku podczas zabiegu z nakłuciem igłą! Zmień nastawienie dziecka do zabiegów medycznych! Możesz użyć do tego swojego smartfona już kilka minut przed zabiegiem i w trakcie zabiegu nakłucia igłą. Aplikacja <b>Magic Spheres</b> ma pięć scenerii i kilka poziomów trudności, które<b> zaprojektowano, aby odwrócić uwagę dziecka od bólu. </b>Pobierz aplikację Magic Spheres i <b>sprawdź, jak rozproszenie uwagi dziecka od ukłucia igłą, zmniejsza jego ból i stres. </b>
-                </p>
-                <div class="download">
-                        <div class="button-text">
-                                Pobierz pełną wersję gry
-                        </div>
-                        <a href='https://play.google.com/store/apps/details?id=com.UWr.MagicSpheres&hl=pl&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'><img class="button" alt='pobierz z Google Play' src='https://play.google.com/intl/pl/badges/static/images/badges/pl_badge_web_generic.png'/>
-                        </a>
-                </div>
-                <div class="download">
-                        <div class="button-text">
-                                Pobierz pełną wersję gry
-                        </div>
-                        <a href='https://play.google.com/store/apps/details?id=com.UWr.MagicSpheres&hl=pl&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'><img class="button" alt='pobierz z Google Play' src='https://play.google.com/intl/pl/badges/static/images/badges/pl_badge_web_generic.png'/>
-                        </a>
-                </div>
-            </div>  
-        </div>
 
-            
-        </section>
-    <div class="wrapper">
-        
-        <section id="section_2"></section>
-        <section id="section_3"></section>
-        <section id="section_4"></section>
-        <section id="section_5"></section>
-        <section id="section_6"></section>
-        <section id="section_7"></section>
+    <div class="header" id="sticky">
+            <img class="logo" src="media/multi/logo.png"></img>
+            <nav class="nav_links">
+                <li><a href="#section_1">O grze</a></li>
+                <li><a href="#section_2">Instrukcja gry</a></li>
+                <li><a href="#section_3">Dlaczego warto</a></li>
+                <li><a href="#section_4">O bólu</a></li>
+                <li><a href="#section_5">O nas</a></li>
+                <li><a href="#section_6">Zaufali nam</a></li>
+                <li><a href="#section_7">Kontakt</a></li>
+            </nav>
+        </header>
     </div>
+    <div class="section_0">
+        <video src="media/multi/gameplay_loop.mp4" playsinline autoplay muted loop></video>
+        <div class="bg_color"></div>
+        <div class="text_wrap">
+            <div class="centered">
+                <h1>Magic Spheres</h1>
+                <h4>gra rozpraszająca uwagę dziecka</h4>
+                <div class="inner_margin">
+                    <p>Pomóż dziecku podczas zabiegu z nakłuciem igłą! Zmień nastawienie dziecka do zabiegów medycznych! Możesz użyć do tego swojego smartfona już kilka minut przed zabiegiem i w trakcie zabiegu nakłucia igłą. Aplikacja Magic Spheres ma pięć scenerii i kilka poziomów trudności, które zaprojektowano, aby odwrócić uwagę dziecka od bólu. <br>
+                        Pobierz aplikację Magic Spheres i sprawdź, jak rozproszenie uwagi dziecka od ukłucia igłą, zmniejsza jego ból i stres. 
+                        </p>
+
+                        <div class="download_containter">
+                            <div class="download_text">
+                                Pobierz pełną wersję gry
+                            </div>
+                            <a href='https://play.google.com/store/apps/details?id=com.UWr.MagicSpheres&hl=pl&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'><img class="download_button" alt='pobierz z Google Play' src='https://play.google.com/intl/pl/badges/static/images/badges/pl_badge_web_generic.png'/>
+                            </a>
+                        </div>
+                        <div class="check_game">
+                            <p>
+                                Zobacz jak wygląda gra 
+                            </p> 
+                        </div>
+                        <div class="learn_more">
+                            <p>
+                                Dowiedz się więcej
+                            </p> 
+                        </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="script.js"></script>
 </body>
 </html>
